91e240d6de07dc26e05bd9f06823f754f3cc50cd
Kruuse2020 <contacto@Kruuse.cl>
2025-06-09 15:01:48 -0400

    feat: finalize Sentry integration - clean configuration, remove test pages, production ready

--- next.config.ts
+++ next.config.ts
@@ -26,49 +26,7 @@ const baseConfig: NextConfig = {
   }
 };
 
-let configWithPlugins = baseConfig;
-
-// Conditionally enable Sentry configuration only in non-turbo mode or production
-if (
-  !process.env.NEXT_PUBLIC_SENTRY_DISABLED &&
-  process.env.NODE_ENV === 'production'
-) {
-  configWithPlugins = withSentryConfig(configWithPlugins, {
-    // For all available options, see:
-    // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-    // FIXME: Add your Sentry organization and project names
-    org: process.env.NEXT_PUBLIC_SENTRY_ORG,
-    project: process.env.NEXT_PUBLIC_SENTRY_PROJECT,
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
-
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    reactComponentAnnotation: {
-      enabled: true
-    },
-
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: '/monitoring',
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Disable Sentry telemetry
-    telemetry: false
-  });
-}
-
-const nextConfig = configWithPlugins;
-export default withSentryConfig(nextConfig, {
+export default withSentryConfig(baseConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
